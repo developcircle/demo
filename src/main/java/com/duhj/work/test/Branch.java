b460dfcc8273d49a1829f67d21273433f90583d6
wb-dhj602221 <wb-dhj602221@alibaba-inc.com>
2019-08-15 15:08:17 +0800

    duhj_02

--- src/main/java/com/duhj/work/test/Branch.java
+++ src/main/java/com/duhj/work/test/Branch.java
@@ -8,7 +8,7 @@ public class Branch {
 
     public static void main(String[] args) {
 
-        System.out.println("===========duhj_01==========");
+        System.out.println("===========duhj_02==========");
 
     }
 }
